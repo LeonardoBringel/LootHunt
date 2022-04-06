6068db5785d508942c260fcb53e6dd0f43857633
LeonardoBringel <leo.sbringel@gmail.com>
2022-04-06 15:49:06 -0300

    :tada: Begin project

---
+++ README.md
@@ -0,0 +1,3 @@
+# loot_hunt
+
+Mobile app that allows you to easily keep track of free games promotions by consuming a third-party API.
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LootHunt());
+}
+
+class LootHunt extends StatelessWidget {
+  const LootHunt({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

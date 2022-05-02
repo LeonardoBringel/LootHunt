6d1cf859d923e8390686ad6a79e860f4595fd75b
LeonardoBringel <leo.sbringel@gmail.com>
2022-05-02 18:28:43 -0300

    :adhesive_bandage: Adjust imports from routes file

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
-import 'package:loot_hunt/screens/favorite_page.dart';
+import 'package:flutter/material.dart';
 
 import 'screens/description_page.dart';
+import 'screens/favorite_page.dart';
 import 'screens/home_page.dart';
 import 'screens/login_page.dart';
 import 'screens/signup_page.dart';

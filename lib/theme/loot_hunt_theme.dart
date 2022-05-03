42b83747353d785d6e34e3b65cb0d4839e9c0816
LeonardoBringel <leo.sbringel@gmail.com>
2022-05-03 12:46:31 -0300

    :recycle: Extract text theme to a function

--- lib/theme/loot_hunt_theme.dart
+++ lib/theme/loot_hunt_theme.dart
@@ -13,33 +13,35 @@ ThemeData lootHuntTheme() {
     tertiary: black,
   );
 
+  TextTheme textTheme = const TextTheme(
+    headline4: TextStyle(
+      fontSize: 34,
+      color: rajah,
+    ),
+    subtitle1: TextStyle(
+      fontSize: 24,
+      color: mustard,
+      fontWeight: FontWeight.bold,
+    ),
+    subtitle2: TextStyle(
+      fontSize: 24,
+      color: mustard,
+    ),
+    bodyText1: TextStyle(
+      fontSize: 18,
+      color: mustard,
+    ),
+    bodyText2: TextStyle(
+      fontSize: 16,
+      color: mustard,
+      fontStyle: FontStyle.italic,
+    ),
+  );
+
   return ThemeData(
     colorScheme: colorScheme,
     scaffoldBackgroundColor: colorScheme.background,
     fontFamily: 'Bebas Neue',
-    textTheme: const TextTheme(
-      headline4: TextStyle(
-        fontSize: 34,
-        color: rajah,
-      ),
-      subtitle1: TextStyle(
-        fontSize: 24,
-        color: mustard,
-        fontWeight: FontWeight.bold,
-      ),
-      subtitle2: TextStyle(
-        fontSize: 24,
-        color: mustard,
-      ),
-      bodyText1: TextStyle(
-        fontSize: 18,
-        color: mustard,
-      ),
-      bodyText2: TextStyle(
-        fontSize: 16,
-        color: mustard,
-        fontStyle: FontStyle.italic,
-      ),
-    ),
+    textTheme: textTheme,
   );
 }

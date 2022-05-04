cabae0d94dfcddd8c1c3c96ea34d12422e0c8c06
LeonardoBringel <leo.sbringel@gmail.com>
2022-05-03 23:32:22 -0300

    :lipstick: Improve description page UI

--- lib/screens/description_page.dart
+++ lib/screens/description_page.dart
@@ -27,8 +27,12 @@ class _DescriptionPageState extends State<DescriptionPage> {
         child: const Icon(Icons.favorite),
         onPressed: () {
           ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Loot added to favorites!'),
+            SnackBar(
+              content: Text(
+                'Loot added to favorites!',
+                style: theme.textTheme.bodyText1,
+              ),
+              backgroundColor: theme.colorScheme.background,
             ),
           );
           Navigator.pop(context, loot.id);
@@ -46,53 +50,90 @@ class _DescriptionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Center(
-      child: Column(
-        children: [
-          Text(
-            loot.title,
-            style: theme.textTheme.subtitle1,
-          ),
-          Image.network(loot.thumbnail),
-          Text(
-            loot.description,
-            style: theme.textTheme.bodyText1,
-          ),
-          ElevatedButton(
-            child: Text(
-              'Instructions',
-              style: theme.textTheme.button,
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+              child: Text(
+                loot.title,
+                style: theme.textTheme.subtitle1,
+              ),
             ),
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text(
-                      'Instructions',
-                      style: theme.textTheme.subtitle2,
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2.0,
+                    color: theme.colorScheme.secondary,
+                  ),
+                  color: theme.colorScheme.tertiary,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    Image.network(loot.thumbnail),
+                    Padding(
+                      padding: const EdgeInsets.all(40),
+                      child: Text(
+                        loot.description,
+                        style: theme.textTheme.bodyText1,
+                      ),
                     ),
-                    content: Text(
-                      loot.instructions,
+                    Text(
+                      'Available at ' + loot.platforms + ' until ' + loot.date,
                       style: theme.textTheme.bodyText1,
                     ),
-                    actions: [
-                      TextButton(
-                        child: Text(
-                          'Done!',
-                          style: theme.textTheme.button,
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 80, 20, 80),
+              child: ElevatedButton(
+                child: Text(
+                  'Instructions',
+                  style: theme.textTheme.button,
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text(
+                          'Instructions',
+                          style: theme.textTheme.subtitle2?.copyWith(
+                            color: theme.colorScheme.secondary,
+                          ),
                         ),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ],
+                        content: Text(
+                          loot.instructions,
+                          style: theme.textTheme.bodyText1,
+                        ),
+                        actions: [
+                          TextButton(
+                            child: Text(
+                              'Done!',
+                              style: theme.textTheme.button?.copyWith(
+                                color: theme.colorScheme.secondary,
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      );
+                    },
                   );
                 },
-              );
-            },
-          ),
-        ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

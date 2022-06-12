c03620647564ad1c365e66cad3f637b3c8f224ff
LeonardoBringel <leo.sbringel@gmail.com>
2022-06-12 13:46:24 -0300

    :recycle: Modify loot widget to use container widget

--- lib/components/loot_widget.dart
+++ lib/components/loot_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/loot.dart';
+import 'container_widget.dart';
 
 class LootWidget extends StatelessWidget {
   const LootWidget({Key? key, required this.loot}) : super(key: key);
@@ -10,50 +11,39 @@ class LootWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container(
-      margin: const EdgeInsets.fromLTRB(40, 40, 40, 0),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 2.0,
-          color: theme.colorScheme.secondary,
+    return ContainerWidget(
+      children: [
+        Text(
+          loot.title,
+          style: theme.textTheme.subtitle1,
         ),
-        color: theme.colorScheme.tertiary,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      height: 380,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text(
-            loot.title,
-            style: theme.textTheme.subtitle1,
-          ),
-          Image.network(loot.thumbnail),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                loot.price,
-                style: theme
-                    .copyWith(
-                      textTheme: const TextTheme(
-                        bodyText2: TextStyle(
-                          decoration: TextDecoration.lineThrough,
-                        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+          child: Image.network(loot.thumbnail),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              loot.price,
+              style: theme
+                  .copyWith(
+                    textTheme: const TextTheme(
+                      bodyText2: TextStyle(
+                        decoration: TextDecoration.lineThrough,
                       ),
-                    )
-                    .textTheme
-                    .bodyText2,
-              ),
-              Text(
-                loot.platforms,
-                style: theme.textTheme.bodyText1,
-              ),
-            ],
-          ),
-        ],
-      ),
+                    ),
+                  )
+                  .textTheme
+                  .bodyText2,
+            ),
+            Text(
+              loot.platforms,
+              style: theme.textTheme.bodyText1,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }

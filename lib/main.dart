7a55c25a070a6232a7c02875235755e96dc2c765
LeonardoBringel <leo.sbringel@gmail.com>
2022-06-13 14:14:57 -0300

    :sparkles: Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:loot_hunt/firebase_options.dart';
 
 import 'theme/loot_hunt_theme.dart';
 import 'routes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const LootHuntApp());
 }
 
